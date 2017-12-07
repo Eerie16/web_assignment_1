46578162bf517fb11356b8f32e0b35262c33f122
Eerie16 <33527566+Eerie16@users.noreply.github.com>
2017-12-07 20:58:30 +0530

    simplified the approach for throttling
    Implemented a very simple and understandable logic without having the need to add event handlers and specific functions for throttling

--- myscript.js
+++ myscript.js
@@ -1,24 +1,16 @@
-//The function for throttling
-function throttle() {
-  var time = Date.now();
-  return function() {
-    if ((Date.now()-time-2000)>0) {
-      increaseFontSize();
-      time = Date.now();
-    }
-  }
-}
-throttleBtn = document.getElementById('b1');
-
-//adding eventlistner for mouseclick at the button 1.
-throttleBtn.addEventListener('click', throttle()); 
+var time=Date.now() -2000; // initialize a time variable with time as 2 second before now() to allow the very first click.
 
 //Function for increasing the font size and the number of clicks
-function increaseFontSize() {
+function increaseFontSize() 
+{
+	var curr=Date.now();	// create a local variable that stores the time when function executes
+	if((curr-time-2000)>0)	// check if the difference between the two time variables is greater than 2 seconds
+	{
+	time=curr;		// if yes, then change the font size, no. of clicks and update the time variable to the value curr.
 	obj = document.getElementById('demo');
 	currentSize = parseFloat(obj.style.fontSize); 
-	document.getElementById('supp').value=parseInt(document.getElementById('supp').value,10)+1;
-	obj.innerHTML="Number of Clicks: "+parseInt(document.getElementById('supp').value,10);
+	document.getElementById('supp').value = parseInt(document.getElementById('supp').value, 10 ) + 1;
+	obj.innerHTML="Number of Clicks: "+ parseInt(document.getElementById('supp').value , 10);
 	obj.style.fontSize = (currentSize + .1) + "em";
-	
+	}
 }
