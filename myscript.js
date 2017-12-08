5a38232e04ef121bec503d54e7556383d2331735
Eerie16 <33527566+Eerie16@users.noreply.github.com>
2017-12-08 11:33:46 +0530

    updated the script to use setTimout() function

--- myscript.js
+++ myscript.js
@@ -1,16 +1,24 @@
-var time=Date.now() -2000; // initialize a time variable with time as 2 second before now() to allow the very first click.
+var check=1;	//Variable that indicates if increaseFontSize() can be called or not. 1 means can be called and 0 means not.
 
 //Function for increasing the font size and the number of clicks
 function increaseFontSize() 
 {
-	var curr=Date.now();	// create a local variable that stores the time when function executes
-	if((curr-time-2000)>0)	// check if the difference between the two time variables is greater than 2 seconds
-	{
-	time=curr;		// if yes, then change the font size, no. of clicks and update the time variable to the value curr.
 	obj = document.getElementById('demo');
 	currentSize = parseFloat(obj.style.fontSize); 
-	document.getElementById('supp').value = parseInt(document.getElementById('supp').value, 10 ) + 1;
-	obj.innerHTML="Number of Clicks: "+ parseInt(document.getElementById('supp').value , 10);
+	document.getElementById('supp').value=parseInt(document.getElementById('supp').value,10)+1;
+	obj.innerHTML="Number of Clicks: "+parseInt(document.getElementById('supp').value,10);
 	obj.style.fontSize = (currentSize + .1) + "em";
-	}
+	
+	
 }
+//This function controls whether increaseFontSize() is called or not
+function caller()
+{
+	if(check==1)	//if increaseFontSize() can be called
+	{	increaseFontSize();	//call it
+		check=0;	//set check to 0 so it cannot be called 
+		setTimeout(function(){check=1;}, 2000);	//until the next 2 seconds
+	}
+	
+}
+	
